a45d5dc97de918d41edb8e63f362e8988afce1c7
jacoluiz <valdineiaribaslunardi@gmail.com>
2023-07-10 20:23:44 -0300

    Alteração feita por mim o chapolin colorado

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,35 +6,36 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import elements.HomePageElementsMap;
 
 public class HomePage extends HomePageElementsMap {
-	public void validarHome () {
+
+	public HomePage() {
 		PageFactory.initElements(driver, this);
+	}
+
+	public void validarHome() {
 		wait.until(ExpectedConditions.visibilityOf(loadingOn));
 		wait.until(ExpectedConditions.invisibilityOf(loadingOn));
 	}
-	
+
 	public void clicarBtnSpecialOffers() {
 		wait.until(ExpectedConditions.elementToBeClickable(btnSpecialOffer));
 		btnSpecialOffer.click();
-		aguardaScroll ();
+		aguardaScroll();
 	}
-	
+
 	public void clicarBtnSeeOffer() {
 		wait.until(ExpectedConditions.elementToBeClickable(btnSeeOffer));
 		btnSeeOffer.click();
 	}
-	
+
 	public void abrirPesquisa() {
-		
+
 		wait.until(ExpectedConditions.elementToBeClickable(btnSearch));
 		btnSearch.click();
 	}
-	
+
 	public void pesquisarProduto() {
 		wait.until(ExpectedConditions.visibilityOf(searchImputText));
 		searchImputText.sendKeys(massa.getNAME_PRODUCT());
-		
 	}
-	
-	
-}
 
+}
